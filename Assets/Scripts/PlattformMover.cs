d7f221bc7f3530809bd643e5aad3fadf2ace2f43
IbbMakesGames <skoldselma0@gmail.com>
2023-10-25 18:21:26 +0200

    tiles

--- Assets/Scripts/PlattformMover.cs
+++ Assets/Scripts/PlattformMover.cs
@@ -11,10 +11,13 @@ public class PlattformMover : MonoBehaviour
     public float moveFreq;
     public float moveDis;
 
+    private Rigidbody2D rb;
+
     Transform objectParent;
     void Start()
     {
         startPos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
@@ -22,16 +25,16 @@ public class PlattformMover : MonoBehaviour
         {
 
             movePos.x = startPos.x + Mathf.Sin(Time.time * moveFreq) * moveDis;
-            transform.position = new Vector2(movePos.x, transform.position.y);
-
-
+            //transform.position = new Vector2(movePos.x, transform.position.y);
+            rb.position = (new Vector2(movePos.x, transform.position.y));
 
 
         }
         if (pathType == path.Vertical)
         {
             movePos.y = startPos.y + Mathf.Sin(Time.time * moveFreq) * moveDis;
-            transform.position = new Vector2(transform.position.x, movePos.y);
+            //transform.position = new Vector2(transform.position.x, movePos.y);
+            rb.MovePosition(new Vector2(transform.position.x, movePos.y));
         }
 
     }
